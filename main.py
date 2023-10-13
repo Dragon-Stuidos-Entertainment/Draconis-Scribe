82b1c8994a859c12165e1cbe5eb9df0b09a3d156
Office Of Naval Intelligence <142657201+OFFICEOFNAVALINTELLIGENCE@users.noreply.github.com>
2023-10-13 09:53:53 -0600

    updated

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 # Load all extensions (cogs) from the "cogs" directory
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}')
+        await bot.load_extension(f'cogs.{filename[:-3]}')  # Use "await" here
 
 @bot.event
 async def on_ready():
