ef3946d7acd1b49c823921a8f8447ee6c0e4d48d
Office Of Naval Intelligence <142657201+OFFICEOFNAVALINTELLIGENCE@users.noreply.github.com>
2023-10-14 13:08:16 -0600

    added channels to read

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -7,17 +7,17 @@ class Welcome(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        # This function will be called when a new member joins the server
         channel_id = 1162823959725740103  # Replace with the actual channel ID where you want to send the welcome message
 
-        # Get the channel by ID
+        channel_to_read = "#enlist-now", "#how-to-join", "about-us"  # Replace with the actual channel name you want to mention
+
         channel = self.bot.get_channel(channel_id)
 
         if channel:
             # Create an embed for the welcome message
-            embed = discord.Embed(title="Welcome to the Office of Naval Intelligence", color=discord.Color(0xFFFFFF))  # White color
-            embed.set_thumbnail(url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLg5IvbPpE5enQ-696wFW74R3pUfZw-Mp-yUyPk6vaGw&s")  # Replace with the URL of the ONI logo image
-            embed.description = f"Welcome, {member.mention}! Thank you for joining our server. Make sure you fill out a Application, to enlist with ONI. to do so go to the enlist-now channel, and read the ways to apply."
+            embed = discord.Embed(title="Welcome to the Office of Naval Intelligence", color=discord.Color(0xFFFFFF))
+            embed.set_thumbnail(url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLg5IvbPpE5enQ-696wFW74R3pUfZw-Mp-yUyPk6vaGw&s")
+            embed.description = f"Welcome, {member.mention}! Thank you for joining our server. Make sure you fill out an Application, to enlist with ONI. To do so, go to the {channel_to_read} channel and read the instructions on how to apply."
 
             await channel.send(embed=embed)
 
