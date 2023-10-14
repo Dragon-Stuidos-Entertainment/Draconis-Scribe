97fb9779d169d29ad61cdb389a62eb893093d46a
Office Of Naval Intelligence <142657201+OFFICEOFNAVALINTELLIGENCE@users.noreply.github.com>
2023-10-14 14:59:48 -0600

    fixed indention

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -42,4 +42,30 @@ class Moderation(commands.Cog):
     async def unmute(self, ctx, member: discord.Member):
         """Unmute a previously muted member."""
         muted_role = discord.utils.get(ctx.guild.roles, name="Muted")  # Replace with your muted role name
-        if muted_role
+        if muted_role is None:
+            await ctx.send("Muted role not found. Please create one.")
+            return
+
+        # Remove the "Muted" role from the member
+        await member.remove_roles(muted_role)
+
+        # Allow the member to talk in voice channels
+        for channel in ctx.guild.voice_channels:
+            await member.edit(mute=False)
+
+        await ctx.send(f"{member.mention} has been unmuted")
+
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def clear(self, ctx, amount: int = 5):
+        """Clear a specified number of messages in the channel."""
+        await ctx.channel.purge(limit=amount + 1)  # +1 to include the command message
+
+    @commands.command()
+    async def ping(self, ctx):
+        """Check the bot's latency."""
+        latency = round(self.bot.latency * 1000)  # Convert to milliseconds
+        await ctx.send(f"Pong! Bot latency is {latency}ms")
+
+def setup(bot):
+    bot.add_cog(Moderation(bot))
