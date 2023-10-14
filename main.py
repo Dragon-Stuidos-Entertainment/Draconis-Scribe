422aac985699546f4f1ad5b7befd6afa65a9a64e
Office Of Naval Intelligence <142657201+OFFICEOFNAVALINTELLIGENCE@users.noreply.github.com>
2023-10-14 10:54:33 -0600

    update

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import discord
 from discord.ext import commands
-import asyncio
 
 intents = discord.Intents.default()
 intents.typing = False
@@ -10,14 +9,13 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 # Load all extensions (cogs) from the "cogs" directory
-
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name} ({bot.user.id})')
-    await bot.load_extension("cogs.ping")
-    await bot.load_extension("cogs.clear")
 
-# Your other event functions and commands here
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            bot.load_extension(f'cogs.{filename[:-3]}')
 
 # Read the bot token from the environment variable
 BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
