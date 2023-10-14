7b121a2bafd3407d0a37fd3ddddca0c735176cf8
Office Of Naval Intelligence <142657201+OFFICEOFNAVALINTELLIGENCE@users.noreply.github.com>
2023-10-14 11:44:08 -0600

    u

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import discord
 from discord.ext import commands
+import datetime
 import asyncio
 
 intents = discord.Intents.default()
@@ -10,15 +11,15 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 # Load all extensions (cogs) from the "cogs" directory
-async def load_cogs():
-    bot.load_extension("cogs.ping")
-    bot.load_extension("cogs.clear")
+def load_extensions():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            bot.load_extension(f'cogs.{filename[:-3]}')
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name} ({bot.user.id})')
-    await load_cogs()
-    print("Cogs loaded successfully")
+    load_extensions()  # Load all extensions (cogs)
 
 # Your other event functions and commands here
 
