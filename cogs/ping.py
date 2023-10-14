4864efba8cb039ef90e792d4f4e2b8a4f67eca84
Office Of Naval Intelligence <142657201+OFFICEOFNAVALINTELLIGENCE@users.noreply.github.com>
2023-10-14 11:42:31 -0600

    updated

---
+++ cogs/ping.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+import datetime
+
+class Ping(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ping(self, ctx):
+        before = datetime.datetime.now()
+        message = await ctx.send("Pinging...")
+        after = datetime.datetime.now()
+        latency = (after - before).total_seconds() * 1000
+        await message.edit(content=f"Pong! Latency is {latency:.2f}ms")
+
+def setup(bot):
+    bot.add_cog(Ping(bot))
