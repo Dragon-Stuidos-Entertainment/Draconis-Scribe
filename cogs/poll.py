20bb6d23e8e6a8bc565b08b24649dd82a6629be3
Office Of Naval Intelligence <142657201+OFFICEOFNAVALINTELLIGENCE@users.noreply.github.com>
2023-10-14 15:58:59 -0600

    updated poll

--- cogs/poll.py
+++ cogs/poll.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands import Context
-import asyncio
+import asyncio  # Add this import for handling duration
 
 class Poll(commands.Cog):
     def __init__(self, bot):
@@ -9,7 +9,7 @@ class Poll(commands.Cog):
         self.active_polls = {}  # Store active polls in this dictionary
 
     @commands.command(name="create_poll")
-    async def create_poll(self, ctx: Context, duration: int, question, *options):
+    async def create_poll(self, ctx: Context, question, duration: int, *options):
         # Create a new poll
         if ctx.author.id in self.active_polls:
             await ctx.send("You already have an active poll. Finish or cancel it before creating a new one.")
@@ -27,47 +27,14 @@ class Poll(commands.Cog):
         for index, option in enumerate(options):
             poll_message += f"{index + 1}. {option}\n"
 
-        poll_message += f"\nVote using the command: !vote <option_number> within {duration} seconds"
-        poll_message = await ctx.send(poll_message)
+        poll_message += "\nVote using the command: !vote <option_number>"
+        await ctx.send(poll_message)
 
-        # Add reaction emojis to the poll message
-        for i in range(1, len(options) + 1):
-            emoji = f"{i}\N{COMBINING ENCLOSING KEYCAP}"
-            await poll_message.add_reaction(emoji)
-
-        # Set a timer to end the poll
+        # Set up a timer to automatically finish the poll after the specified duration
         await asyncio.sleep(duration)
-        await self.finish_poll(ctx.author.id, poll_message)
+        await self.finish_poll(ctx)
 
-    async def finish_poll(self, author_id, poll_message):
-        if author_id not in self.active_polls:
-            return
-
-        poll_data = self.active_polls.pop(author_id)
-        poll = poll_data
-        results = {option: 0 for option in poll["options"]}
-
-        for vote in poll["votes"].values():
-            results[poll["options"][vote - 1]] += 1
-
-        result_message = "Poll Results:\n"
-        for option, count in results.items():
-            result_message += f"{option}: {count} votes\n"
-
-        await poll_message.edit(content=result_message)
-
-    @commands.Cog.listener()
-    async def on_reaction_add(self, reaction, user):
-        if user.bot:
-            return  # Ignore reactions by the bot
-
-        for author_id, poll in self.active_polls.items():
-            if reaction.message.content.startswith(poll["question"]):
-                emoji = reaction.emoji
-                emoji_str = emoji[0] if not emoji.isnumeric() else emoji
-                if emoji_str in poll["options"]:
-                    poll["votes"][user.id] = poll["options"].index(emoji_str) + 1
-                    return
+    # ... (other commands remain the same)
 
 def setup(bot):
     bot.add_cog(Poll(bot))
