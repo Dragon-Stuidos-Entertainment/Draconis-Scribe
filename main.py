dc6354daaf84d453eb1a95a147589e503fc3a821
Office Of Naval Intelligence <142657201+OFFICEOFNAVALINTELLIGENCE@users.noreply.github.com>
2023-10-14 19:46:25 -0600

    Updated main file

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import discord
 from discord.ext import commands
-import asyncio
 
 intents = discord.Intents.default()
 intents.typing = False
@@ -36,6 +35,26 @@ async def on_disconnect():
     channel = bot.get_channel(your_channel_id)
     if channel:
         await channel.send("Bot is undergoing maintenance and is now offline.")
+
+@bot.command(name="help")
+async def help_command(ctx, command_name: str = None):
+    if not command_name:
+        # If no specific command is specified, list all available commands
+        embed = discord.Embed(title="Command List", color=discord.Color.blue())
+        for cog in bot.cogs:
+            commands_list = bot.get_cog(cog).get_commands()
+            if commands_list:
+                command_names = [command.name for command in commands_list]
+                embed.add_field(name=cog, value=" | ".join(command_names), inline=False)
+        await ctx.send(embed=embed)
+    else:
+        # If a specific command is specified, provide information about that command
+        command = bot.get_command(command_name)
+        if not command:
+            await ctx.send("Command not found.")
+        else:
+            embed = discord.Embed(title=f"Help: {command.name}", description=command.help, color=discord.Color.blue())
+            await ctx.send(embed=embed)
 
 # Your other event functions and commands here
 
