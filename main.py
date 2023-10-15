840531b1abba7d89e6b4be536aad3a4a8022cac3
Office Of Naval Intelligence <142657201+OFFICEOFNAVALINTELLIGENCE@users.noreply.github.com>
2023-10-14 19:25:35 -0600

    updated main

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import discord
 from discord.ext import commands
-import asyncio
 
 intents = discord.Intents.default()
 intents.typing = False
@@ -10,15 +9,21 @@ intents.members = True
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+# Define the load_extensions function before calling it
+def load_extensions():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            bot.load_extension(f'cogs.{filename[:-3]}')
+
 # Your event decorators go here
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name} ({bot.user.id})')
     load_extensions()  # Load all extensions (cogs)
-    
+
     # Replace this with your channel ID
     your_channel_id = 1118420639947173959
-    
+
     channel = bot.get_channel(your_channel_id)
     if channel:
         await channel.send("Bot is online and ready for action!")
@@ -27,18 +32,12 @@ async def on_ready():
 async def on_disconnect():
     # Replace this with your channel ID
     your_channel_id = 1118420639947173959
-    
+
     channel = bot.get_channel(your_channel_id)
     if channel:
         await channel.send("Bot is undergoing maintenance and is now offline.")
 
 # Your other event functions and commands here
-
-# Load all extensions (cogs) from the "cogs" directory
-def load_extensions():
-    for filename in os.listdir('./cogs'):
-        if filename.endswith('.py'):
-            bot.load_extension(f'cogs.{filename[:-3]}')
 
 # Read the bot token from the environment variable
 BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
