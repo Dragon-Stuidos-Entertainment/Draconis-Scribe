c77a7a7d53e1a811c70c9d695df0a408d589e332
Office Of Naval Intelligence <142657201+OFFICEOFNAVALINTELLIGENCE@users.noreply.github.com>
2023-10-16 09:12:55 -0600

    Updated automoderation

--- cogs/automoderation.py
+++ cogs/automoderation.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 
+# Define a decorator for the cooldown
+@commands.cooldown(1, 10)  # 1 use every 10 seconds
+
 class AutoModeration(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -19,22 +22,37 @@ class AutoModeration(commands.Cog):
             if time_difference < 5:
                 # User is sending messages too quickly
                 if time_difference < 3:
-                    # User is sending messages very quickly, mute the user
-                    muted_role = discord.utils.get(message.guild.roles, name="Muted")  # Replace with your muted role name
-                    if muted_role:
-                        await message.author.add_roles(muted_role)
-                        await message.channel.send(f"{user.mention} has been muted for spamming.")
-                    else:
-                        await message.channel.send("Muted role not found. Please create one.")
-                
-                # Delete the spammy message
-                await message.delete()
-                # Warn the user
-                warning_message = f"{user.mention}, please refrain from spamming."
-                await message.channel.send(warning_message)
-            
-            
-        self.message_history[user.id] = message.created_at
+                    # Check if the user has the "manage roles" permission
+                    has_manage_roles_permission = any(role.permissions.manage_roles for role in user.roles)
+                    
+                    # Check if the user has the "Officer's" or "Enlisted NCO" roles
+                    has_officer_role = any(role.name == "Officer's" for role in user.roles)
+                    has_enlisted_nco_role = any(role.name == "Enlisted NCO" for role in user.roles)
+
+                    # Apply the cooldown for specific roles
+                    if not (has_manage_roles_permission or has_officer_role or has_enlisted_nco_role):
+                        await self.trigger_moderation_actions(message)
+
+                # Update the message history
+                self.message_history[user.id] = message.created_at
+
+    async def trigger_moderation_actions(self, message):
+        user = message.author
+
+        # User is sending messages very quickly, mute the user
+        muted_role = discord.utils.get(message.guild.roles, name="Muted")  # Replace with your muted role name
+        if muted_role:
+            await message.author.add_roles(muted_role)
+            await message.channel.send(f"{user.mention} has been muted for spamming.")
+        else:
+            await message.channel.send("Muted role not found. Please create one.")
+        
+        # Delete the spammy message
+        await message.delete()
+
+        # Warn the user
+        warning_message = f"{user.mention}, please refrain from spamming."
+        await message.channel.send(warning_message)
 
 def setup(bot):
     bot.add_cog(AutoModeration(bot))
