84b47f7f4db5fed8eea2bb4540a521706b3859af
Office Of Naval Intelligence <142657201+OFFICEOFNAVALINTELLIGENCE@users.noreply.github.com>
2023-10-16 11:00:25 -0600

    Updated

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 class Moderation(commands.Cog):
-    def __init(self, bot):
+    def __init__(self, bot):
         self.bot = bot
 
     async def log_moderator_command(self, ctx, command_name, target):
