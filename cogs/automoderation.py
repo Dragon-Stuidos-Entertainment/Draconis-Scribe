f5dd1f05505ae6e183d46cf2c63eed987230f133
Office Of Naval Intelligence <142657201+OFFICEOFNAVALINTELLIGENCE@users.noreply.github.com>
2023-10-15 19:30:16 -0600

    update

--- cogs/automoderation.py
+++ cogs/automoderation.py
@@ -32,9 +32,7 @@ class AutoModeration(commands.Cog):
                 # Warn the user
                 warning_message = f"{user.mention}, please refrain from spamming."
                 await message.channel.send(warning_message)
-            elif time_difference < 3:
-                # User is sending messages very quickly, consider additional actions
-                # You've already muted the user in this case, but you can add more actions if needed.
+            
             
         self.message_history[user.id] = message.created_at
 
